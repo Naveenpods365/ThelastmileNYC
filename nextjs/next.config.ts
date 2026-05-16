9fe22adaf8b1e32d3392086667e2ee648f4977ff
Harsh-i-t <hitman.harsh21@gmail.com>
2026-05-16 16:52:14 +0530

    some changes

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: "export", // Static export for Netlify deployment
     trailingSlash: true,
     productionBrowserSourceMaps: true,
     images: {
